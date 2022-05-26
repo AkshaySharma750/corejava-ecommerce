867162b91dbd5e9a3898548eb2bb344fc4490ab6
Akshay Sharma <Akshay@Akshay.SHARMA>
2022-05-26 19:21:24 +0530

    initial commit

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		List<String> names = Arrays.asList("Appple","Mango","Grapes");
+
+		for(Integer i =0; i<names.size(); i++) {
+			names.set(2,"Banana");
+		}
+		System.out.println(names);
+	}
+
+}
